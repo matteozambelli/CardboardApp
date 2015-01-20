f6ddb14352fff16409c7c49bdb3ee24803e03f77
matteozambelli <teo.zambe@gmail.com>
2015-01-20 14:35:22 +0100

    inserita verisione con gestore input

--- CardboardPB/app/src/main/java/com/example/fabio/cardboardpb/MainActivity.java
+++ CardboardPB/app/src/main/java/com/example/fabio/cardboardpb/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.fabio.cardboardpb;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.KeyEvent;
 
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -36,4 +41,90 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                //on key + press
+                if (action == KeyEvent.ACTION_DOWN) {
+                    volumeUp();
+                }
+                return true;
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+                //on key + press
+                if (action == KeyEvent.ACTION_DOWN) {
+                    volumeUp();
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    //on key - press
+                    volumeDown();
+                }
+                return true;
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    //on key - press
+                    volumeDown();
+                }
+                return true;
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+                if(action== KeyEvent.ACTION_DOWN){
+                //on key home press
+                home();
+            }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
+    }
+
+
+    /**
+     * handle the key + press event
+     */
+    private void volumeUp(){
+
+        new AlertDialog.Builder(this)
+                .setTitle("test mode")
+                .setMessage("key + pressed").setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // continue with delete
+            }
+        }).show();
+
+    }
+
+    /**
+     * handle the key - press event
+     */
+    private void volumeDown(){
+        new AlertDialog.Builder(this)
+                .setTitle("test mode")
+                .setMessage("key - pressed").setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // continue with delete
+          }
+}).show();
+        }
+
+/**
+ * handle the home button pression
+ */
+private void home(){
+
+    new AlertDialog.Builder(this)
+            .setTitle("test mode")
+            .setMessage("key home pressed").setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            // continue with delete
+        }
+    }).show();
+
+    }
+
 }

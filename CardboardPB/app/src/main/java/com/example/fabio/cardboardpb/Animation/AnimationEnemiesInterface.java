f3a36160315d0eb722980f7578c501de2704b438
matteozambelli <teo.zambe@gmail.com>
2015-01-27 12:23:12 +0100

    aggiunte interface

--- CardboardPB/app/src/main/java/com/example/fabio/cardboardpb/Animation/AnimationEnemiesInterface.java
+++ CardboardPB/app/src/main/java/com/example/fabio/cardboardpb/Animation/AnimationEnemiesInterface.java
@@ -1,7 +1,21 @@
 package com.example.fabio.cardboardpb.Animation;
 
+import android.view.animation.AnimationSet;
+import android.widget.ImageView;
+
 /**
  * Created by matteo on 27/01/2015.
  */
 public interface AnimationEnemiesInterface {
+
+    public AnimationSet animateFrontCarLane1(final ImageView ivLeft, final ImageView ivRight) ;
+
+
+    public AnimationSet animateFrontCarLane2(final ImageView ivLeft, final ImageView ivRight);
+
+    public AnimationSet animateFrontCarLane3(final ImageView ivLeft, final ImageView ivRight);
+
+    public void hideImage(ImageView img);
+
+    public void showImage(ImageView img);
 }

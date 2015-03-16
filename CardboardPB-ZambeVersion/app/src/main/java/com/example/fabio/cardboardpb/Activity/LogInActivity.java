f2b55aa1c3a47af8696d9dcc7497b654c278e46c
Matteo Zambelli <teo.zambe@gmail.com>
2015-03-16 16:30:04 +0100

    commit pc fisso zambe

--- CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/Activity/LogInActivity.java
+++ CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/Activity/LogInActivity.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,9 +19,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.fabio.cardboardpb.DB.PostCall;
 import com.example.fabio.cardboardpb.Manager.Enum.TypeCall;
 import com.example.fabio.cardboardpb.Manager.PasswdManager;
@@ -44,7 +49,7 @@ public class LogInActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
 
 
         logInActivity = this;
@@ -58,16 +63,16 @@ public class LogInActivity extends Activity {
         passwordToSend = PasswdManager.calculateHash(password.toString());
         play = (Button) findViewById(R.id.playWithoutReg);
         forgot = (Button) findViewById(R.id.forgotPassword);
-        status =(TextView) findViewById(R.id.status);
+        status = (TextView) findViewById(R.id.status);
 
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 passwordToSend = PasswdManager.calculateHash(password.getText().toString());
-                post = new PostCall(email.getText().toString(), passwordToSend,status);
+                post = new PostCall(email.getText().toString(), passwordToSend, status);
                 post.myPostCall(TypeCall.LOG_IN, logInActivity);
-                if(status.getText().toString().contains("password errata")){
+                if (status.getText().toString().contains("password errata")) {
                     wrongPasswordAlert();
                 }
                 launchRingDialog();
@@ -125,14 +130,14 @@ public class LogInActivity extends Activity {
         final EditText firstName = new EditText(this);
         final EditText lastName = new EditText(this);
         final EditText eMail = new EditText(this);
-        final EditText answare = new EditText(this);
+
         final EditText password = new EditText(this);
         final EditText confirmPassword = new EditText(this);
-        final Spinner spinner = new Spinner(this);
+
         LinearLayout layout = new LinearLayout(this);
         final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        ArrayAdapter<CharSequence> adapter_gg = ArrayAdapter.createFromResource(this, R.array.security_question, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter_gg);
+
+
         alert.setMessage("SIGN UP");
 
         if (firstname.equals("") && lastname.equals("") && email.equals("")) {
@@ -175,7 +180,9 @@ public class LogInActivity extends Activity {
             lastName.setText(lastname);
             eMail.setText(email);
         }
-        answare.setHint("your answare");
+
+
+
         password.setHint("password");
         confirmPassword.setHint("confirm password");
         password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
@@ -186,10 +193,11 @@ public class LogInActivity extends Activity {
         layout.addView(firstName);
         layout.addView(lastName);
         layout.addView(eMail);
+
         layout.addView(password);
         layout.addView(confirmPassword);
-        //  layout.addView(answare);
-        // layout.addView(spinner);
+
+
         alert.setView(layout);
 
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
@@ -213,8 +221,8 @@ public class LogInActivity extends Activity {
                     //Cript the password
                     passwordToSend = PasswdManager.calculateHash(password.getText().toString());
                     //send data
-                    post=new PostCall(firstName.getText().toString(),lastName.getText().toString(),eMail.getText().toString(),passwordToSend,status);
-                    post.myPostCall(TypeCall.SIGN_UP,logInActivity);
+                    post = new PostCall(firstName.getText().toString(), lastName.getText().toString(), eMail.getText().toString(), passwordToSend, status);
+                    post.myPostCall(TypeCall.SIGN_UP, logInActivity);
                 }
             }
         });
@@ -241,7 +249,7 @@ public class LogInActivity extends Activity {
             public void onClick(DialogInterface dialog, int which) {
                 launchRingDialog();
                 passwordToSend = PasswdManager.calculateHash("default_user");
-                post = new PostCall("3d4ambcardboard@gmail.com", passwordToSend,status);
+                post = new PostCall("3d4ambcardboard@gmail.com", passwordToSend, status);
                 post.myPostCall(TypeCall.LOG_IN, logInActivity);
             }
         });
@@ -270,6 +278,10 @@ public class LogInActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
+
+
+
+
             }
         });
         alert.setNegativeButton("Cancel",
@@ -293,19 +305,11 @@ public class LogInActivity extends Activity {
 
             }
         });
-        alert.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Canceled.
-                    }
-                });
-
         alert.show();
     }
 
-    public void launchRingDialog() {
-        final ProgressDialog ringProgressDialog = ProgressDialog.show(LogInActivity.this, "Please wait ...",	"contacting server ...", true);
+    private void launchRingDialog() {
+        final ProgressDialog ringProgressDialog = ProgressDialog.show(LogInActivity.this, "Please wait ...", "contacting server ...", true);
         ringProgressDialog.setCancelable(true);
         new Thread(new Runnable() {
             @Override
@@ -315,24 +319,31 @@ public class LogInActivity extends Activity {
                     // Let the progress ring for 10 seconds...
 
                     Thread.sleep(2000);
-                    id_user= status.getText().toString().substring(11,12);
+                    id_user = status.getText().toString().substring(11, 12);
 
-                    if(status.getText().toString().contains("connection")) {
+                    if (status.getText().toString().contains("connection")) {
                         Intent i = new Intent(LogInActivity.this, SplashActivity.class);
                         i.putExtra("id_user", id_user);
                         startActivity(i);
-                    }
-                    if(status.getText().toString().contains("password errata")){
-                        forgotPasswordAlert();
-                    }
+                    } else if (status.getText().toString().contains("password errata")) {
+                        logInActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                wrongPasswordAlert();
+                            }
+                        });
 
+
+                    }
                 } catch (Exception e) {
 
                 }
                 ringProgressDialog.dismiss();
             }
         }).start();
+
     }
 
 
 }
+

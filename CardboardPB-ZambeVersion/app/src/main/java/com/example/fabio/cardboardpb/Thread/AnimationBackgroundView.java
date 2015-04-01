fe3a3f53a8fe6f0301bccb98390dfb8eaef8bf2b
Fabio <f.terzi1@studenti.unibg.it>
2015-04-01 17:35:36 +0200

    cartoonBackground

--- CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/Thread/AnimationBackgroundView.java
+++ CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/Thread/AnimationBackgroundView.java
@@ -1,10 +1,13 @@
 package com.example.fabio.cardboardpb.Thread;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -12,6 +15,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.fabio.cardboardpb.R;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class AnimationBackgroundView extends SurfaceView {
     private SurfaceHolder holder;
@@ -57,8 +63,14 @@ public class AnimationBackgroundView extends SurfaceView {
     }
 
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void createSprite() {
-        Bitmap bmpBackground = BitmapFactory.decodeResource(getResources(), R.drawable.sfondo8);
+        Bitmap bmpBackground = BitmapFactory.decodeResource(getResources(), R.drawable.cartoonbackground1);
+        //bmpBackground.getConfig();
+        //mutableBitmap.reconfigure(400,400, mutableBitmap.getConfig());
+        //ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //bmpBackground.compress(Bitmap.CompressFormat.PNG,100,out);
+        //Bitmap decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
         spriteBackground = new Sprite(this,bmpBackground,1);
     }
 

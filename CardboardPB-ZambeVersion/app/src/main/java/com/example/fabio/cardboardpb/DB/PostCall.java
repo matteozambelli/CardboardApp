229152533602b45ad605052ed245238f5fb10ab4
matteozambelli <teo.zambe@gmail.com>
2015-04-07 15:16:09 +0200

    update icona

--- CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/DB/PostCall.java
+++ CardboardPB-ZambeVersion/app/src/main/java/com/example/fabio/cardboardpb/DB/PostCall.java
@@ -3,15 +3,9 @@ package com.example.fabio.cardboardpb.DB;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.fabio.cardboardpb.Manager.Enum.TypeCall;
-
-import junit.framework.Test;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -21,10 +15,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -53,7 +45,7 @@ public class PostCall {
     private String id_doctor;
     //UPDATE
     private String update;
-
+    private String reverseString;
 
     TextView status;
 
@@ -157,86 +149,88 @@ public class PostCall {
                     List<NameValuePair> nameValuePairsSignUp = new ArrayList<NameValuePair>(7);
                     List<NameValuePair> nameValuePairsDoctorCall = new ArrayList<NameValuePair>(5);
                     List<NameValuePair> nameValuePairsReport = new ArrayList<NameValuePair>(4);
-                    if (type.equals(TypeCall.LOG_IN)) {
-                        nameValuePairsLogIn.add(new BasicNameValuePair("type", "log_in"));
-                        nameValuePairsLogIn.add(new BasicNameValuePair("email", email));
-                        nameValuePairsLogIn.add(new BasicNameValuePair("password", password));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsLogIn));
-                    }
-                    if (type.equals(TypeCall.SIGN_UP)) {
-                        nameValuePairsSignUp.add(new BasicNameValuePair("type", "sign_up"));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("first_name", firstName));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("last_name", lastName));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("email", email));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("color", myColor));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("date", birthday));
-                        nameValuePairsSignUp.add(new BasicNameValuePair("password", password));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsSignUp));
-                    }
-                    if (type.equals(TypeCall.RESET)) {
-                        nameValuePairsReset.add(new BasicNameValuePair("type", "reset"));
-                        nameValuePairsReset.add(new BasicNameValuePair("email", email));
-                        nameValuePairsReset.add(new BasicNameValuePair("color", myColor));
-                        nameValuePairsReset.add(new BasicNameValuePair("new_password", newPassword));
-                        nameValuePairsReset.add(new BasicNameValuePair("new_passwordCfr", passwordCfr));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsReset));
-                    }
-                    if (type.equals(TypeCall.DOCTORCALL)) {
-                        nameValuePairsDoctorCall.add(new BasicNameValuePair("type", "doctorcall"));
-                        nameValuePairsDoctorCall.add(new BasicNameValuePair("first_name", firstName));
-                        nameValuePairsDoctorCall.add(new BasicNameValuePair("last_name", lastName));
-                        nameValuePairsDoctorCall.add(new BasicNameValuePair("id_doctor", id_doctor));
-                        nameValuePairsDoctorCall.add(new BasicNameValuePair("birthday", birthday));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsDoctorCall));
-                    }
-                    if (type.equals(TypeCall.REPORT)) {
-                        nameValuePairsReport.add(new BasicNameValuePair("type", "report"));
-                        nameValuePairsReport.add(new BasicNameValuePair("score", score));
-                        nameValuePairsReport.add(new BasicNameValuePair("level", level));
-                        nameValuePairsReport.add(new BasicNameValuePair("id_user", id_user));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsReport));
-                    }
-                    if (type.equals(TypeCall.UPDATE_MAIL)) {
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_email"));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("email", update));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
+                    if(!type.equals(TypeCall.REPORT)) {
+                        if (type.equals(TypeCall.LOG_IN)) {
+                            nameValuePairsLogIn.add(new BasicNameValuePair("type", "log_in"));
+                            nameValuePairsLogIn.add(new BasicNameValuePair("email", email));
+                            nameValuePairsLogIn.add(new BasicNameValuePair("password", password));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsLogIn));
+                        }
+                        if (type.equals(TypeCall.SIGN_UP)) {
+                            nameValuePairsSignUp.add(new BasicNameValuePair("type", "sign_up"));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("first_name", firstName));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("last_name", lastName));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("email", email));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("color", myColor));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("date", birthday));
+                            nameValuePairsSignUp.add(new BasicNameValuePair("password", password));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsSignUp));
+                        }
+                        if (type.equals(TypeCall.RESET)) {
+                            nameValuePairsReset.add(new BasicNameValuePair("type", "reset"));
+                            nameValuePairsReset.add(new BasicNameValuePair("email", email));
+                            nameValuePairsReset.add(new BasicNameValuePair("color", myColor));
+                            nameValuePairsReset.add(new BasicNameValuePair("new_password", newPassword));
+                            nameValuePairsReset.add(new BasicNameValuePair("new_passwordCfr", passwordCfr));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsReset));
+                        }
+                        if (type.equals(TypeCall.DOCTORCALL)) {
+                            nameValuePairsDoctorCall.add(new BasicNameValuePair("type", "doctorcall"));
+                            nameValuePairsDoctorCall.add(new BasicNameValuePair("first_name", firstName));
+                            nameValuePairsDoctorCall.add(new BasicNameValuePair("last_name", lastName));
+                            nameValuePairsDoctorCall.add(new BasicNameValuePair("id_doctor", id_doctor));
+                            nameValuePairsDoctorCall.add(new BasicNameValuePair("birthday", birthday));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsDoctorCall));
+                        }
+                        if (type.equals(TypeCall.REPORT)) {
+                            nameValuePairsReport.add(new BasicNameValuePair("type", "report"));
+                            nameValuePairsReport.add(new BasicNameValuePair("score", score));
+                            nameValuePairsReport.add(new BasicNameValuePair("level", level));
+                            nameValuePairsReport.add(new BasicNameValuePair("id_user", id_user));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsReport));
+                        }
+                        if (type.equals(TypeCall.UPDATE_MAIL)) {
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_email"));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("email", update));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
 
-                    }
-                    if (type.equals(TypeCall.UPDATE_FIRSTNAME)) {
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_firstname"));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("firstname", update));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
+                        }
+                        if (type.equals(TypeCall.UPDATE_FIRSTNAME)) {
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_firstname"));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("firstname", update));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
 
-                    }
-                    if (type.equals(TypeCall.UPDATE_LASTNAME)) {
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_lastname"));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("lastname", update));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
+                        }
+                        if (type.equals(TypeCall.UPDATE_LASTNAME)) {
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_lastname"));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("lastname", update));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
 
-                    }
-                    if (type.equals(TypeCall.UPDATE_BIRTHDAY)) {
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_birthday"));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("birthday", update));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
-                    }
-                    if (type.equals(TypeCall.UPDATE_PASSWORD)) {
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_password"));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
-                        nameValuePairsUpdateString.add(new BasicNameValuePair("password", update));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
+                        }
+                        if (type.equals(TypeCall.UPDATE_BIRTHDAY)) {
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_birthday"));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("birthday", update));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
+                        }
+                        if (type.equals(TypeCall.UPDATE_PASSWORD)) {
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("type", "update_password"));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("id_user", id_user));
+                            nameValuePairsUpdateString.add(new BasicNameValuePair("password", update));
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairsUpdateString));
 
+                        }
+
+                        // Execute HTTP Post Request
+                        final ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+                        response = httpclient.execute(httppost, responseHandler);
+                        //This is the response from a php application
+                        reverseString = response;
                     }
-
-                    // Execute HTTP Post Request
-                    final ResponseHandler<String> responseHandler = new BasicResponseHandler();
-
-                    response = httpclient.execute(httppost, responseHandler);
-                    //This is the response from a php application
-                    final String reverseString = response;
                     activity.runOnUiThread(new Runnable() {
                         public void run() {
                              status.setText(reverseString);
